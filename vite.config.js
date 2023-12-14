902cf79834e37d4b7068652a925d665d405fcf46
mac <a12@Shahils-MacBook-Pro.local>
2023-12-14 15:49:05 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/react3/",
+  base:"/Part_3-lab-react-think-with-react/",
   plugins: [react()],
 })
